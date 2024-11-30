64350d2300cb331cb0420128ebfd32c026e77d50
IvanPAVLOV <144616500+PavlovIvan1@users.noreply.github.com>
2024-11-30 12:13:37 +0300

    Update App.jsx

--- twif-react-app/src/App.jsx
+++ twif-react-app/src/App.jsx
@@ -13,23 +13,10 @@ export function App() {
 
   const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   if (window.Telegram.WebApp.platform == "tdesktop" || window.Telegram.WebApp.platform == 'macos' || window.Telegram.WebApp.initData.length == 0) {
-  //     navigate('/mobapp')
-  //   }
-  //   else {
-  //     window.Telegram.WebApp.ready();
-  //     window.Telegram.WebApp.setHeaderColor("bg_color");
-
-  //     if (window.Telegram.WebApp.isVersionAtLeast(8.0)) {
-  //       window.Telegram.WebApp.requestFullscreen();
-  //     } else {
-  //       console.warn("Fullscreen API недоступен");
-  //     }
-  //   }
-  // }, []);
-
-    useEffect(() => {
+  useEffect(() => {
+    if (window.Telegram.WebApp.platform == "tdesktop" || window.Telegram.WebApp.platform == 'macos' || window.Telegram.WebApp.initData.length == 0) {
+      // navigate('/mobapp')
+    }
       window.Telegram.WebApp.ready();
       window.Telegram.WebApp.setHeaderColor("bg_color");
 
@@ -38,7 +25,6 @@ export function App() {
       } else {
         console.warn("Fullscreen API недоступен");
       }
-    }
   }, []);
 
 
