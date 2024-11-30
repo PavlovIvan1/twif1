2c5c75372181c46de16bca69bfd32bb51d2ea4dc
IvanPAVLOV <144616500+PavlovIvan1@users.noreply.github.com>
2024-11-30 20:57:03 +0300

    Update CreateParty.jsx

--- twif-react-app/src/Pages/CreateParty/CreateParty.jsx
+++ twif-react-app/src/Pages/CreateParty/CreateParty.jsx
@@ -21,7 +21,7 @@ export function ControlLabel({ checked, onChange }) {
           color="primary"
         />
       }
-      
+
     />
   );
 }
@@ -73,7 +73,7 @@ export function CreatePartyComp() {
 
   // Функция для обработки файла из FileUploader
   const handleFileUpload = (file) => {
-      setUploadedFile(file); // Сохраняем файл в стейт родителя
+    setUploadedFile(file); // Сохраняем файл в стейт родителя
   };
 
 
@@ -87,14 +87,12 @@ export function CreatePartyComp() {
 
   const handleTwifChange = (event) => {
     setTwifChecked(event.target.checked);
-    console.log(twifChecked)
   };
 
   const [selectNftValue, setselectNftValue] = useState("")
 
   const select_value = (event) => {
     setselectNftValue(event.target.value)
-    console.log(event.target.value)
   }
 
   let [title, setTitle] = useState('h')
@@ -102,97 +100,87 @@ export function CreatePartyComp() {
 
   const titleChange = (event) => {
     setTitle(event.target.value)
-    console.log(event.target.value)
   }
   const quantityChange = (event) => {
     setQuantity(event.target.value)
-    console.log(event.target.value)
   }
 
   const [nftCollections, setnftCollections] = useState(null);
-	
-	useEffect(() => {
-		fetch(` ${API_URL}/nft/collection/colors`)
-			.then(response => response.json())
-			.then(data => {
-				console.log(data.colors)
-				setnftCollections(data.colors)
-			})
-			.catch(error => {
+
+  useEffect(() => {
+    fetch(` ${API_URL}/nft/collection/colors`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.colors)
+        setnftCollections(data.colors)
+      })
+      .catch(error => {
         console.error('Error fetching user data:', error);
       });
 
-	}, [])
+  }, [])
 
-    const [title2, setT2] = useState("Title")
+  const [title2, setT2] = useState("Title")
 
-    const calert = () => {
-      toast.success("Success!");
+  const calert = () => {
+    toast.success("Success!");
+  }
+
+  const create_party_btn = () => {
+    var formData = new FormData();
+
+    if (uploadedFile) {
+      console.log('uploadedFile exists');
+      formData.append('logo', uploadedFile);
+    }
+    else {
+      formData = null
     }
 
-    const create_party_btn = () => {
-      console.log(uploadedFile);
-      console.log("Title and q", title, quantity)
-  
-      const formData = new FormData();
-      formData.append('logo', uploadedFile);
-  
-      console.log(window.Telegram.WebApp.initData)
+    const data = {
+      initData: window.Telegram.WebApp.initData,
+      title: title,
+      quantity: quantity,
+      founder_share: 0.3,
+      members_share: 0.4,
+      project_share: 0.2,
+      voters_share: 0.1
+    };
 
-      const data = {
-        initData: window.Telegram.WebApp.initData,
-        title: title,
-        quantity: quantity,
-        founder_share: 0.3,
-        members_share: 0.4,
-        project_share: 0.2,
-        voters_share: 0.1
-      };
-    
-      const params = new URLSearchParams(data).toString();
+    const params = new URLSearchParams(data).toString();
 
-      fetch(` ${API_URL}/party/create?${params}`, {
-        method: 'POST',
-        headers: {
-          'accept': 'application/json'
-        },
-        body: formData
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log("Ответ от сервера:", data.detail);
-          if (data.detail) {
+    fetch(`${API_URL}/party/create?${params}`, {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        if (!response.ok) {
+          response.json().then(errData => {
             ToastErr.fire({
               icon: "error",
-              title: data.detail
+              title: errData.detail || 'Ошибка при создании'
             });
-          } else {
-            Toast.fire({
-              icon: "success",
-              title: "Created!"
-            });
-          } 
-          navigate("/parties")
-        })
-        .catch((error) => {
-          console.error("Err:", error)
-          ToastErr.fire({
-            icon: "error",
-            title: "Somph went wrong!"
           });
-        })
-    }
+        }
+        else {
+          Toast.fire({
+            icon: "success",
+            title: "Created!"
+          });
+          navigate("/parties")
+        };
+      })
+  }
 
-
-	if (!nftCollections) {
-		return (
-			<div className="spinner"></div>
-		)
-	}
+  if (!nftCollections) {
+    return (
+      <div className="spinner"></div>
+    )
+  }
 
   return (
     <>
-     <ToastContainer
+      <ToastContainer
         position="top"
         autoClose={2000}
         hideProgressBar
@@ -203,76 +191,76 @@ export function CreatePartyComp() {
         draggable
         pauseOnFocusLoss
       />
-    <div className={styles.CreateNewParty}>
-      <FileUploader onFileSelect={handleFileUpload} />
-      {/* {uploadedFile && <p>Выбранный файл: {uploadedFile.name}</p>} */}
-      <div className={styles.title_field}>
-        <label>Title</label>
-        <input type="text" onChange={titleChange} />
-      </div>
-      <div className={styles.title_field}>
-        <label>Quantity</label>
-        <input type="text" onChange={quantityChange} />
-      </div>
-
-      <div className={styles.chs_nft}>
-        <div className={styles.top_nft}>
-          <h3>NFT</h3>
-          <ControlLabel 
-            checked={nftChecked} 
-            onChange={handleNftChange} 
-          />
+      <div className={styles.CreateNewParty}>
+        <FileUploader onFileSelect={handleFileUpload} />
+        {/* {uploadedFile && <p>Выбранный файл: {uploadedFile.name}</p>} */}
+        <div className={styles.title_field}>
+          <label>Title</label>
+          <input type="text" onChange={titleChange} />
         </div>
-        {/* {nftChecked && (
+        <div className={styles.title_field}>
+          <label>Quantity</label>
+          <input type="text" onChange={quantityChange} />
+        </div>
+
+        <div className={styles.chs_nft}>
+          <div className={styles.top_nft}>
+            <h3>NFT</h3>
+            <ControlLabel
+              checked={nftChecked}
+              onChange={handleNftChange}
+            />
+          </div>
+          {/* {nftChecked && (
           <select className={styles.selectNFT__list}>
             <option disabled value="Choise">Choose NFT</option>
             <option value="NFT1">NFT1</option>
           </select>
         )} */}
-        {nftChecked && (
-          <select className={styles.selectNFT__list} onChange={select_value}>
-            <option>NONE</option>
+          {nftChecked && (
+            <select className={styles.selectNFT__list} onChange={select_value}>
+              <option>NONE</option>
               {nftCollections.map((collection, index) => (
                 <option key={index} value={collection}>
                   {collection}
                 </option>
               ))}
-          </select>
+            </select>
+          )}
+
+        </div>
+
+        <div className={styles.chs_twif}>
+          <h3>TWIF</h3>
+          <ControlLabel
+            checked={twifChecked}
+            onChange={handleTwifChange}
+          />
+        </div>
+
+        {twifChecked && (
+          <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" className={styles.slider} />
         )}
-
       </div>
-
-      <div className={styles.chs_twif}>
-        <h3>TWIF</h3>
-        <ControlLabel 
-          checked={twifChecked} 
-          onChange={handleTwifChange} 
-        />
+      <div className={styles.create}>
+        <button onClick={create_party_btn} style={{ cursor: 'pointer', backgroundColor: 'white', borderRadius: '12px', width: '95%', color: '#51B3F6', height: '56px', border: 'none', fontFamily: 'Inter', fontSize: '17px', fontWeight: '500' }}>Create</button>
+        {/* <h3 onClick={create_party_btn} >Create</h3> */}
       </div>
-
-			{ twifChecked && (
-			 <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" className={styles.slider} />
-			)}
-    </div>
-    <div className={styles.create}>
-      <button onClick={create_party_btn} style={{ cursor: 'pointer', backgroundColor: 'white', borderRadius: '12px', width: '95%', color: '#51B3F6', height: '56px', border: 'none', fontFamily: 'Inter', fontSize: '17px', fontWeight: '500' }}>Create</button>
-      {/* <h3 onClick={create_party_btn} >Create</h3> */}
-    </div>
     </>
   );
 }
 
 export function CreateParty() {
   const navigate = useNavigate()
-  
-  
+
+
   return (
     <div className={styles.pages_bg}>
       <div className={styles.title}>
-        <img src="/Group 36866.png" alt="back" onClick={() => {navigate('/parties')}} />
+        <img src="/Group 36866.png" alt="back" onClick={() => { navigate('/parties') }} />
         <h2>Create new party</h2>
       </div>
-      
+
       <CreatePartyComp />
     </div>
   );
