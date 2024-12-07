3d213f43798893cc3abaf5491d006ae8466a199b
IvanPAVLOV <144616500+PavlovIvan1@users.noreply.github.com>
2024-12-07 20:50:03 +0300

    Update App.jsx

--- twif-react-app/src/App.jsx
+++ twif-react-app/src/App.jsx
@@ -19,7 +19,7 @@ export function App() {
     }
     window.Telegram.WebApp.ready();
     window.Telegram.WebApp.setHeaderColor("bg_color");
-    window.Telegram.WebApp.isVerticalSwipesEnabled(false)
+    window.Telegram.WebApp.disableVerticalSwipes()
 
     // if (window.Telegram.WebApp.isVersionAtLeast(8.0)) {
     //   window.Telegram.WebApp.requestFullscreen();
